f5e1a7fa6f357290916a3d3f8dbe7d99d3ba37ee
webmo-lalday <lalday@web-mo.com>
2026-09-16 13:17:50 -0700

    copy react-template

---
+++ client/src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+
+import './css/index.css'
+import './css/static.css'
+import './css/defaults.css'
+import './css/colorpicker.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
---
+++ client/src/static/Nav.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom"
+
+export default function Nav() {
+    return (
+        <nav>
+            <div className="icon">&#9776;</div>
+            <div className="nav-links">
+                <Link className="nav-link" to='/'>Home</Link>
+                <Link className="nav-link" to='/style'>Stylesheet</Link>
+                <div className="dropdown">
+                    <div className="nav-link disabled">Content</div>
+                    <div className="dropdown-content">
+                        <Link className="nav-link" to='/'>Nested</Link>
+                        <Link className="nav-link" to='/'>Nested</Link>
+                    </div>
+                </div>
+                <div className="dropdown">
+                    <div className="nav-link disabled">Content</div>
+                    <div className="dropdown-content">
+                        <Link className="nav-link" to='/'>Nested</Link>
+                        <Link className="nav-link" to='/'>Nested</Link>
+                    </div>
+                </div>
+                <Link className="nav-link" to='/'>Home</Link>
+                <Link className="nav-link" to='/'>Home</Link>
+                <Link className="nav-link" to='/'>Home</Link>
+            </div>
+        </nav>
+    )
+}
